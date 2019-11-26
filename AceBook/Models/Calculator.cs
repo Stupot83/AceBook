54ec66df1f51320f97311dbab0edb55f35acdb51
Shahrukh Khan <Shahrukh.Khan.2597@gmail.com>
2019-11-26 14:57:25 +0000

    added example test file and calculator class

---
+++ AceBook/Models/Calculator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace AceBook.Models
+{
+    public class Calculator
+    {
+        public int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public int Minus(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+
+        public int Multiply(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+
+        public float Divide(float num1, float num2)
+        {
+            return num1 / num2;
+        }
+    }
+}
---
+++ AceBookTests/ExampleTests.cs
@@ -0,0 +1,39 @@
+using NUnit.Framework;
+using AceBook.Models;
+
+namespace AceBookTests
+{
+    [TestFixture]
+    public class CalculatorTest
+    {
+        Calculator sut;
+        [SetUp]
+        
+
+        public void TestSetup()
+        {
+            sut = new Calculator();
+        }
+
+        [Test]
+        public void ShouldAddTwoNumbers()
+        {
+            int expectedResult = sut.Add(10, 8);
+            Assert.That(expectedResult, Is.EqualTo(18));
+        }
+
+        [Test]
+        public void ShouldMultiplyTwoNumbers()
+        {
+            int expectedResult = sut.Multiply(10, 10);
+            Assert.That(expectedResult, Is.EqualTo(100));
+        }
+
+        [Test]
+        public void ShouldDivideTwoNumbers()
+        {
+            float expectedResult = sut.Divide(10, 10);
+            Assert.That(expectedResult, Is.EqualTo(1));
+        }
+    }
+}
