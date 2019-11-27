8ccd7c5d38dc758da2e188455a665b1c347187cb
Shahrukh Khan <Shahrukh.Khan.2597@gmail.com>
2019-11-27 10:57:02 +0000

    :construction: Set up DB Helper

---
+++ AceBook/Helpers/DbHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Configuration;
+using System.Collections.Generic;
+using System.IO;
+using AceBook.Models;
+using System.Drawing;
+using System.Web;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace AceBook.Helpers
+{
+    public class DbHelper
+    {
+        private static IMongoCollection<BsonDocument> ConnectToDB(string dbName, string collectionName)
+        {
+            var connectionString = "mongodb://localhost:27017";
+
+            var client = new MongoClient(MongoUrl.Create(connectionString));
+
+            var database = client.GetDatabase(dbName);
+
+            return database.GetCollection<BsonDocument>(collectionName);
+        }
+    }
+}
