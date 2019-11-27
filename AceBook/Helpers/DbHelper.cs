73c0cd106bbe5365e1c537c6ef23328ced0a5586
Shahrukh Khan <Shahrukh.Khan.2597@gmail.com>
2019-11-27 13:58:07 +0000

    added register user to dbhelper

--- AceBook/Helpers/DbHelper.cs
+++ AceBook/Helpers/DbHelper.cs
@@ -12,15 +12,32 @@ namespace AceBook.Helpers
 {
     public class DbHelper
     {
-        private static IMongoCollection<BsonDocument> ConnectToDB(string dbName, string collectionName)
+        private static IMongoCollection<BsonDocument> ConnectToDB(string AceBookDB, string collectionName)
         {
             var connectionString = "mongodb://localhost:27017";
 
             var client = new MongoClient(MongoUrl.Create(connectionString));
 
-            var database = client.GetDatabase(dbName);
+            var database = client.GetDatabase(AceBookDB);
 
             return database.GetCollection<BsonDocument>(collectionName);
+        }
+
+        public static void RegisterUser(string firstName, string lastName, string email, string password, string phoneNumber, string birthDate, string gender)
+        {
+            var collection = ConnectToDB("AceBookDb", "user");
+            var document = new BsonDocument
+            {
+                { "firstName", firstName },
+                { "lastName", lastName },
+                { "email", email },
+                { "password", PasswordHasher.Hash(password) },
+                { "phoneNumber", phoneNumber },
+                { "birthDate", birthDate },
+                { "gender", gender }
+            };
+
+            collection.InsertOneAsync(document);
         }
     }
 }
