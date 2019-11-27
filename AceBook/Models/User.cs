18eabd7121b3d4e52273300f8d3da19fa4b64e06
6joeb <joe6ball@gmail.com>
2019-11-27 10:56:30 +0000

    user phone number test passed

--- AceBook/Models/User.cs
+++ AceBook/Models/User.cs
@@ -7,5 +7,6 @@ namespace AceBook.Models
         public string LastName { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
+        public string PhoneNumber { get; set; }
     }
 }
