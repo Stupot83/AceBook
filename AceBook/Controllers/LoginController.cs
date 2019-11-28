08e816877013848b98f4c15aa3b03fd4bc4e563a
Stuart Priestley <Stuart.Priestley@bglgroup.co.uk>
2019-11-28 10:25:55 +0000

    :construction: Build login controller and basic view

---
+++ AceBook/Controllers/LoginController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using AceBook.Helpers;
+
+namespace AceBook.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult LoginUser(string email, string password)
+        {
+            if (DbHelper.GetUser(email, password))
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            return RedirectToAction("Login", "User");
+        }
+
+        public IActionResult LogOut()
+        { 
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
