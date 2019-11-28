40899f93228b1099301963e62a5e1cb26f33efe6
Stuart Priestley <Stuart.Priestley@bglgroup.co.uk>
2019-11-28 12:28:40 +0000

    :construction::white_check_mark: Add logic for login controller and all tests passing

--- AceBookTests/LoginTest.cs
+++ AceBookTests/LoginTest.cs
@@ -26,15 +26,48 @@ namespace AceBookTests
         [Test]
         public void FillInFormAndLogin()
         {
-            var url = "http://localhost:5000/account/login";
+            var url = "https://localhost:5001/Account/Login";
 
             _driver.Navigate().GoToUrl(url);
 
-            _driver.FindElement(By.CssSelector("#email")).SendKeys("83@a.com");
+            
+            _driver.FindElement(By.CssSelector("#email")).SendKeys("a@a.com");
             _driver.FindElement(By.CssSelector("#password")).SendKeys("c");
             _driver.FindElement(By.CssSelector("#submit")).Click();
 
-            Assert.That(_driver.PageSource, Does.Contain("Welcome to Acebook"));
+            Assert.That(_driver.PageSource, Does.Contain("Acebook"));
+        }
+
+        [Test]
+        public void CheckRedirect()
+        {
+            var url = "https://localhost:5001/Account/Login";
+
+            _driver.Navigate().GoToUrl(url);
+
+            _driver.FindElement(By.CssSelector("#email")).SendKeys("a@a.com");
+            _driver.FindElement(By.CssSelector("#password")).SendKeys("c");
+            _driver.FindElement(By.CssSelector("#submit")).Click();
+
+            var expectedUrl = _driver.Url;
+
+            Assert.AreEqual("https://localhost:5001/", expectedUrl);
+        }
+
+        [Test]
+        public void CheckNonRedirect()
+        {
+            var url = "https://localhost:5001/Account/Login";
+
+            _driver.Navigate().GoToUrl(url);
+
+            _driver.FindElement(By.CssSelector("#email")).SendKeys("b@b.com");
+            _driver.FindElement(By.CssSelector("#password")).SendKeys("a");
+            _driver.FindElement(By.CssSelector("#submit")).Click();
+
+            var expectedUrl = _driver.Url;
+
+            Assert.AreEqual("https://localhost:5001/Account/Login", expectedUrl);
         }
     }
 }
