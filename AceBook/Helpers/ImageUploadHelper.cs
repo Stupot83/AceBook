75699b854158311818324d89cdb168e2f3f581f1
jtsalva <tim@jtsalva.dev>
2019-12-05 15:44:42 +0000

    add upload helper

---
+++ AceBook/Helpers/ImageUploadHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using Microsoft.AspNetCore.Http;
+
+namespace AceBook.Helpers
+{
+    public class ImageUploadHelper
+    {
+
+        public static string ImageToBase64(IFormFile image)
+        {
+            using (var ms = new MemoryStream())
+            {
+                image.CopyTo(ms);
+                string base64 = Convert.ToBase64String(ms.ToArray());
+
+                Console.WriteLine("Base64: " + "data:image/gif;base64," + base64);
+
+                return "data:image/gif;base64," + base64;
+            }
+        }
+    }
+}
